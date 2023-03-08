c454d9619f9799fa20efc54144618417beb8166e
Keep Growing And Moving Forward <liuweistat@163.com>
2023-03-08 20:36:49 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+
+
+class BaseNetwork(nn.Module):
+    def __init__(self):
+        super(BaseNetwork, self).__init__()
+
+    def init_weights(self, init_type='normal', gain=0.02):
+        '''
+        initialize network's weights
+        init_type: normal | xavier | kaiming | orthogonal
+        https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/9451e70673400885567d08a9e97ade2524c700d0/models/networks.py#L39
+        '''
+        pass
+      
+      
